9478e40ed2022af2551e4fc0608630e7ec28e171
Eka <6597211+ekafyi@users.noreply.github.com>
2021-05-08 17:25:09 +0700

    perf: lazy-load share component

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,18 +1,29 @@
-import { RWebShare } from "react-web-share";
+import dynamic from "next/dynamic";
+// import { RWebShare } from "react-web-share";
+
+const RWebShare = dynamic(() => import("react-web-share").then((rws) => rws.RWebShare), {
+  ssr: false,
+  loading: () => <ShareButton disabled="true" />,
+});
+
+const ShareButton = (props) => (
+  <button className="text-sm font-medium px-4 py-1 rounded bg-gray-50 text-gray-700 hover:text-black" {...props}>
+    Share
+  </button>
+);
 
 const Share: React.FunctionComponent<{ title: string }> = ({ title }) => {
+  const data = {
+    text: `Check out this playlist: ${title}`,
+    url: typeof window !== "undefined" ? window.location.href : process.env.URL,
+    title: title,
+  };
   return (
     <>
-      <RWebShare
-        data={{
-          text: `Check out this playlist: ${title}`,
-          url: typeof window !== "undefined" ? window.location.href : process.env.URL,
-          title: title,
-        }}
-      >
-        <button className="text-sm font-medium px-4 py-1 rounded bg-gray-50 text-gray-700 hover:text-black">
-          Share
-        </button>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <RWebShare data={data}>
+        <ShareButton />
       </RWebShare>
       <style jsx global>{`
         .web-share-fade header {
