ce8c3c4898067130887fe4a20a8dfb87d2783b36
Eka <6597211+ekafyi@users.noreply.github.com>
2022-01-21 20:24:50 +0700

    remove AnimateSharedLayout from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
-import { AnimateSharedLayout, domMax, LazyMotion } from "framer-motion";
+import { domMax, LazyMotion } from "framer-motion";
 import type { AppProps } from "next/app";
 import "./app.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <LazyMotion features={domMax}>
-    <AnimateSharedLayout type="crossfade">
-      <Component {...pageProps} />
-    </AnimateSharedLayout>
+    <Component {...pageProps} />
   </LazyMotion>
 );
 
