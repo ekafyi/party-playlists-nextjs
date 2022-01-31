7e12e0e9ac394e921f8cabe034c1d03744792bd9
Eka <6597211+ekafyi@users.noreply.github.com>
2022-01-31 20:18:09 +0700

    moar ui fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 
 const Footer: React.FunctionComponent = () => {
   return (
-    <footer className="p-4 text-xs text-gray-800 flex justify-between">
+    <footer className="flex flex-col justify-between gap-4 p-4 text-center text-xs text-gray-800 sm:flex-row">
       <div>Made with ♡ and occasional desperation in 🇮🇩</div>
       <div>
         <a
-          className="underline font-bold hover:text-blue-600"
+          className="font-bold underline hover:text-blue-600"
           href="https://github.com/ekafyi/party-playlists-nextjs"
           rel="external"
         >
