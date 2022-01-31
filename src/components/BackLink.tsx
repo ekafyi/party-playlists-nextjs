5ce184630f71ea6b8ad0ca1e469eac6066c30cc6
Eka <6597211+ekafyi@users.noreply.github.com>
2022-01-31 20:27:03 +0700

    use svg for backlink arrow

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -11,7 +11,15 @@ const BackLink: React.FunctionComponent = () => (
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className={styles.arrow}>&larr;</div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={styles.arrow}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+      </svg>
     </motion.a>
   </Link>
 );
